3d6d441db40d63646edcf81e7c8380ea512517d7
Arnthor Sverrisson <arnthor@Arnthors-MacBook-Pro.local>
2020-04-02 11:15:04 +0200

    Cia's frontend

---
+++ app/static/js/script.js
@@ -0,0 +1,35 @@
+window.onload = function(){
+  n =  new Date();
+  y = n.getFullYear();
+  m = n.getMonth();
+  d = n.getDate();
+
+  var weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+
+  var months = new Array(12);
+  months[0] = "January";
+  months[1] = "February";
+  months[2] = "March";
+  months[3] = "April";
+  months[4] = "May";
+  months[5] = "June";
+  months[6] = "July";
+  months[7] = "August";
+  months[8] = "September";
+  months[9] = "October";
+  months[10] = "November";
+  months[11] = "December";
+
+  var day = weekday[n.getDay()];
+  var month = months[m];
+
+  document.getElementById("date").innerHTML = day + " - " + month + " " + d;
+
+};
---
+++ app/static/js/script2.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $( '#info-button' ).click( function() {
+      $('#diagram').attr("style", "display:block")
+      $('#background-overlay').fadeToggle();
+      $('#close-diagram').fadeToggle();
+      $('.diagram-area').fadeToggle();
+
+  });
+
+  $(document).click(function(e){
+    var container = $('#diagram');
+    var button = $('#info-button');
+
+    // if the target of the click isn't the container nor a descendant of the container
+    if (!container.is(e.target) && !button.is(e.target) && container.has(e.target).length === 0)
+    {
+      $('#diagram').fadeOut(300);
+      $('#background-overlay').fadeOut(300);
+      $('#close-diagram').fadeOut(300);
+      $('.diagram-area').fadeOut(300);
+    }
+  });
+});
