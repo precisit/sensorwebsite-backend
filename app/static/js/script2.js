629b7ccd29b3e509271cf5c5a083bf08f143966b
Arnthor Sverrisson <arnthor@Arnthors-MacBook-Pro.local>
2020-04-06 16:21:08 +0200

    show text from smhi api

--- app/static/js/script2.js
+++ app/static/js/script2.js
@@ -1,4 +1,25 @@
 $(document).ready(function() {
+  var urlCodes = "https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/29/codes.json";
+  var url = "https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/29/station/97530/period/latest-day/data.json";
+  var res = $.getJSON(url, function(json) {
+    var items = [];
+    var val = json.value[0].value;
+    var res = $.getJSON(urlCodes, function(json) {
+        text = json.entry[parseInt(val)].value
+        console.log(text)
+        console.log(val)
+        $('#weather-text').append(text);
+    })
+  })
+  .done(function() {
+    console.log( "Get JSON done" );
+  })
+  .fail(function(jqxhr, textStatus, error) {
+    console.log( "Get JSON error" );
+  })
+  .always(function() {
+    console.log( "Get JSON complete" );
+  });
   $( '#info-button' ).click( function() {
       $('#diagram').attr("style", "display:block")
       $('#background-overlay').fadeToggle();
@@ -6,6 +27,7 @@ $(document).ready(function() {
       $('.diagram-area').fadeToggle();
 
   });
+
 
   $(document).click(function(e){
     var container = $('#diagram');
