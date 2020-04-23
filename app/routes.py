afbd90ff9fe1343b73e207e24afa3eb6cfc26a6b
Arnthor Sverrisson <arnthor@Arnthors-MacBook-Pro.local>
2020-04-23 08:58:44 +0200

    error message when db connection fails

--- app/routes.py
+++ app/routes.py
@@ -6,17 +6,20 @@ from app.models import measurements
 @app.route('/')
 @app.route('/index')
 def index():
-    query = db.session.query(func.max(measurements.timestamp))
-    newest_timestamp = query.one()
-    query2 = db.session.query(measurements).filter(measurements.timestamp == newest_timestamp)
-    m = query2.one()
-    cloudy_limit = 600
-    sunny_limit = 1000
-    cloudy_text = ""
-    if m.ill > sunny_limit:
-        cloudy_text = "Sunny"
-    elif m.ill < cloudy_limit:
-        cloudy_text = "Cloudy"
-    else:
-        cloudy_text = "Partly Cloudy"
-    return render_template('index.html', measurement=m, is_cloudy=cloudy_text)
+    try:
+        query = db.session.query(func.max(measurements.timestamp))
+        newest_timestamp = query.one()
+        query2 = db.session.query(measurements).filter(measurements.timestamp == newest_timestamp)
+        m = query2.one()
+        cloudy_limit = 600
+        sunny_limit = 1000
+        cloudy_text = ""
+        if m.ill > sunny_limit:
+            cloudy_text = "Sunny"
+        elif m.ill < cloudy_limit:
+            cloudy_text = "Cloudy"
+        else:
+            cloudy_text = "Partly Cloudy"
+        return render_template('index.html', measurement=m, is_cloudy=cloudy_text)
+    except:
+        return render_template('error.html', errormessage="Cant'retreve data")
