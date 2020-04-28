68afe9fa442da64a3427c80c8ddb8abe51414719
Arnthor Sverrisson <arnthor@Arnthors-MacBook-Pro.local>
2020-04-28 17:27:31 +0200

    display different cloud images depending on measurements

--- app/main/routes.py
+++ app/main/routes.py
@@ -13,15 +13,20 @@ def index():
         newest_timestamp = query.one()
         query2 = db.session.query(measurements).filter(measurements.timestamp == newest_timestamp)
         m = query2.one()
-        cloudy_limit = 600
-        sunny_limit = 1000
-        cloudy_text = ""
-        if m.ill > sunny_limit:
-            cloudy_text = "Sunny"
-        elif m.ill < cloudy_limit:
-            cloudy_text = "Cloudy"
-        else:
-            cloudy_text = "Partly Cloudy"
+        cloudy_text = get_cloud_text(m.ill)
         return render_template('index.html', measurement=m, is_cloudy=cloudy_text)
     except:
         return render_template('error.html')
+
+
+def get_cloud_text(ill):
+    cloudy_text = ""
+    cloudy_limit = 530
+    sunny_limit = 1100
+    if ill > sunny_limit:
+        cloudy_text = "Sunny"
+    elif ill < cloudy_limit:
+        cloudy_text = "Cloudy"
+    else:
+        cloudy_text = "Partly Cloudy"
+    return cloudy_text
